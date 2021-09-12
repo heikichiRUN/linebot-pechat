2e4d533c6098654478914e7b65318e69254489fe
root <root@DESKTOP-D8D4E2B.localdomain>
2021-09-12 13:50:31 +0900

    -helloworld

---
+++ import pya3rt.py
@@ -0,0 +1,7 @@
+import pya3rt
+
+#def talk_ai()
+apikey = "DZZun7AGQ9C7VYpDqsQWffXWg4xaMGQd"
+client = pya3rt.TalkClient(apikey)
+reply_message = client.talk("いい天気ですね")
+print(reply_message["results"][0]["reply"])
